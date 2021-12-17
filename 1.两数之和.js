e8a03b46943b9066c54aad47d4446404f0854910
lisha.wang <wlsweb@163.com>
2021-12-17 14:22:05 +0800

    fr

---
+++ 1.两数之和.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}fdsf 
+ */
+var twoSum = function (nums, target) {};ss
+// @lc code=endddd
